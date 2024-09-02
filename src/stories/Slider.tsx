f2b5ddbfc62521eee9b5152d4bfb4e000f3bd36e
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-09-03 04:09:33 +0700

    ADD: Slider component

---
+++ src/stories/Slider.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Slider: React.FC = () => {
+    return (
+        <div className="min-w-[500px] min-h-[10px] bg-gray-200 px-2 flex items-center py-2 rounded-md">
+            <button className="min-w-[50px] min-h-[10px] rounded-md bg-night dark:bg-sakura outline-none"></button>
+        </div>
+    )
+}
+
+
+
+export default Slider
