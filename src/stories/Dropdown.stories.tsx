6b70a119f12d3a65505ba3ff7453f6753ecacda1
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-09-03 04:01:59 +0700

    UPDATE: Dropdown stories to display the preview

--- src/stories/Dropdown.stories.tsx
+++ src/stories/Dropdown.stories.tsx
@@ -11,5 +11,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {}
+  args: {
+    defaultValue: "Apple",
+    options: ["Apple", "Mango", "Cherry"]
+  }
 };
