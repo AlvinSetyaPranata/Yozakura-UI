daf6f8b51a2a5a7cedb5f1ad6029a3fa4f486f17
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-09-03 04:02:31 +0700

    UPDATE: Added background Add-ons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,9 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-postcss',
-    '@storybook/addon-themes'
+    '@storybook/addon-themes',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-toolbars',
   ]
 };
 
