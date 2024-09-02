b21c53c900a0b10767905506a4a9f3a3c3ec3ee7
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-09-02 20:23:22 +0700

    UPDATE: Changed secondary color to match theme

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,9 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'dark', // Choose between 'light' or 'dark' as a base theme
   brandTitle: 'Yozakura UI',
-
+  brandImage: 'file:///Users/alvinsetyapranata/Downloads/DALL%C2%B7E%202024-09-02%2020.09.18%20-%20A%20minimalist%20logo%20design%20featuring%20a%20single%20Yozakura%20flower.%20The%20flower%20should%20have%20delicate,%20vibrant%20pale%20pink%20blossoms%20with%20enhanced%20color%20intensity.webp',
   colorPrimary: '#FFC0CB', // Cherry Blossom Pink
-  colorSecondary: '#FFD700', // Warm Gold
-
-
+  colorSecondary: '#FFC0CB',
 });
